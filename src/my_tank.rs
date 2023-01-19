584ce77446648ab3ab7ea39e8ff0958903c55d36
code-tanks <112593882+code-tanks@users.noreply.github.com>
2023-01-19 02:49:21 +0000

    refactor

--- src/my_tank.rs
+++ src/my_tank.rs
@@ -3,20 +3,16 @@ use serde_json::Value;
 
 pub struct MyTank {}
 
-impl TankTraits for MyTank {
-    fn run(&self, commands: &mut Vec<CCommand>) {
-        println!("run");
-        commands.push(CCommands::MOVE_FORWARD);
+impl Tank for MyTank {
+    fn run(&mut self, commands: &mut Vec<CCommand>) {
+        todo!()
     }
 
-    fn on_event(&self, commands: &mut Vec<CCommand>, event: &Value) {
+    fn on_event(&mut self, commands: &mut Vec<CCommand>, event: &Value) {
         todo!()
     }
 }
 
-pub fn create_tank() -> Tank<'static> {
-    Tank {
-        commands: Vec::new(),
-        my_tank:  &MyTank {}
-    }
+pub fn create_tank() -> MyTank {
+    MyTank {}
 }
