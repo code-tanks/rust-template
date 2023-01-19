372305ce3ffdd6ef4e57fed0f0108be7f30cdc4b
code-tanks <112593882+code-tanks@users.noreply.github.com>
2023-01-19 00:57:36 +0000

    update

--- src/my_tank.rs
+++ src/my_tank.rs
@@ -1,14 +1,15 @@
-use ct_api::{Tank, TankTraits};
+use ct_api::*;
 use serde_json::Value;
 
 pub struct MyTank {}
 
 impl TankTraits for MyTank {
-    fn run(&self) {
-        todo!()
+    fn run(&self, commands: &mut Vec<CCommand>) {
+        println!("run");
+        commands.push(CCommands::MOVE_FORWARD);
     }
 
-    fn on_event(&self, event: &Value) {
+    fn on_event(&self, commands: &mut Vec<CCommand>, event: &Value) {
         todo!()
     }
 }
